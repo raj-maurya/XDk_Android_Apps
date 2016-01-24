36d6b26274636dfcee755436baca38fe1b4d5a9d
Raj Maurya <rajkmaurya111@gmail.com>
2016-01-24 22:36:54 +0530

    N-droid/spalsh screen

---
+++ Core1.020.01.16/app/src/main/java/com/skynet/hunter/core10200116/Splash.java
@@ -0,0 +1,37 @@
+package com.skynet.hunter.core10200116;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by hunter on 24/1/16.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle simple_variable) {
+        super.onCreate(simple_variable);
+        setContentView(R.layout.splash);
+        Thread timer = new Thread() {
+            public void run(){
+                try{
+                    sleep(5000);
+
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+
+                }finally {
+                    Intent openStartingPoint = new Intent("com.skynet.hunter.core10200116.MAINACTIVITY");
+                    startActivity(openStartingPoint);
+
+                }
+
+
+            }
+        };
+        timer.start();
+    }
+
+
+}
