6977cde044e62f210461d77210bdf95fc7300b45
Raj Maurya <rajkmaurya111@gmail.com>
2016-01-26 21:51:54 +0530

    N-droid/splash_sound

--- Core1.020.01.16/app/src/main/java/com/skynet/hunter/core10200116/Splash.java
+++ Core1.020.01.16/app/src/main/java/com/skynet/hunter/core10200116/Splash.java
@@ -2,17 +2,21 @@ package com.skynet.hunter.core10200116;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 /**
  * Created by hunter on 24/1/16.
  */
 public class Splash extends Activity {
+    MediaPlayer song;
 
     @Override
     protected void onCreate(Bundle simple_variable) {
         super.onCreate(simple_variable);
         setContentView(R.layout.splash);
+         song = MediaPlayer.create(Splash.this, R.raw.saxophone);
+        song.start();
         Thread timer = new Thread() {
             public void run(){
                 try{
@@ -35,6 +39,7 @@ public class Splash extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        song.release();
         finish();
     }
 
