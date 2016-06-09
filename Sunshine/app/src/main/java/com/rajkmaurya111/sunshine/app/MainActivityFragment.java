fa696d1ec57391cf6a75e4a3f1614138f6b80802
Raj Maurya <rajkmaurya111@gmail.com>
2016-06-09 07:57:10 -0700

    added activity detail option

--- Sunshine/app/src/main/java/com/rajkmaurya111/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/rajkmaurya111/sunshine/app/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.rajkmaurya111.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -11,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -92,7 +95,19 @@ public class MainActivityFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+               // Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
 
+            }
+        }
+
+        );
 
         return rootView;
 
